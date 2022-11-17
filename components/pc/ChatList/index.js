46a7b2cc726768d0e5e7c6db93306ad29005d5fe
whoiseon <whois__@naver.com>
2022-11-17 12:10:34 +0900

    initial commit

--- components/pc/ChatList/index.js
+++
@@ -1,9 +0,0 @@
-const ChatList = () => {
-  return (
-    <>
-      채팅목록
-    </>
-  );
-};
-
-export default ChatList;
--- components/pc/Root/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Index = ({ Component, pageProps }) => {
-  return (
-    <Component Component={Component} pageProps={pageProps} />
-  );
-};
-
-export default Index;
